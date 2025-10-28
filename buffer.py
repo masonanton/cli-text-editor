5220cfcb0a2d381d3fdff94a1c8141ea6e4e7c28
Mason Anton <masonanton1@gmail.com>
2025-10-28 00:00:56 -0500

    Add basic text functionality and cursor movement.
    Added typing, backspace, and enter.
    In each situation the cursor handles all cases, including edge cases,
    correctly.
    Added left and right movements when on the end of a line and moving to
    the previous or next line accordingly.

--- buffer.py
+++ buffer.py
@@ -9,4 +9,23 @@ class TextBuffer:
             "A much longer line to test bounds",
         ]
 
-    # TODO: add/insert operations 
+    def insert_char(self, row, col, char):
+        line = self.lines[row]
+        self.lines[row] = line[:col] + char + line[col:]
+
+    def delete_char(self, row, col):
+        if col > 0:
+            line = self.lines[row]
+            self.lines[row] = line[:col - 1] + line[col:]
+        elif row > 0:
+            prev = self.lines[row - 1]
+            self.lines[row - 1] = prev + self.lines[row]
+            del self.lines[row]
+
+    def insert_newline(self, row, col):
+        line = self.lines[row]
+        self.lines[row] = line[:col]
+        self.lines.insert(row + 1, line[col:])
+
+    def get_prev_line_length(self, row):
+        return len(self.lines[row - 1])
--- editor.py
+++ editor.py
@@ -1,8 +1,12 @@
 import curses
 from buffer import TextBuffer
 
-CTRL_Q_KEY = 17 # Ctrl + Q ASCII
+CTRL_Q_KEY = 17 
+ENTER_KEY = 10
+BACKSPACE_KEY = 8
+DELETE_KEY = 127
 ARROW_KEYS = (curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT)
+DELETION_KEYS = (BACKSPACE_KEY, DELETE_KEY, curses.KEY_BACKSPACE)
 class Editor:
     def __init__(self, stdscr):
         self.stdscr = stdscr
@@ -22,9 +26,24 @@ class Editor:
                 break
             elif key in ARROW_KEYS:
                 self.move_cursor(key)
+            elif key == ENTER_KEY:
+                self.buffer.insert_newline(self.cursor_row, self.cursor_col)
+                self.cursor_row += 1
+                self.cursor_col = 0
+            elif key in DELETION_KEYS:
+                if self.cursor_col == 0 and self.cursor_row > 0:
+                    prev_line_length = len(self.buffer.lines[self.cursor_row - 1])
+                self.buffer.delete_char(self.cursor_row, self.cursor_col)
+                if self.cursor_col > 0:
+                    self.cursor_col -= 1
+                else:
+                    self.cursor_row -= 1
+                    self.cursor_col = prev_line_length
             else:
-                # TODO: insert text logic
-                pass
+                if 32 <= key <= 126:
+                    char = chr(key)
+                    self.buffer.insert_char(self.cursor_row, self.cursor_col, char)
+                    self.cursor_col += 1
 
     def render(self):
         self.stdscr.clear()
@@ -41,10 +60,19 @@ class Editor:
             self.cursor_row -= 1
         elif key == curses.KEY_DOWN and self.cursor_row < len(self.buffer.lines) - 1:
             self.cursor_row += 1
-        elif key == curses.KEY_LEFT and self.cursor_col > 0:
-            self.cursor_col -= 1
-        elif key == curses.KEY_RIGHT and self.cursor_col < len(self.buffer.lines[self.cursor_row]):
-            self.cursor_col += 1
+        elif key == curses.KEY_LEFT: 
+            if self.cursor_col > 0:
+                self.cursor_col -= 1
+            elif self.cursor_row > 0:
+                prev_line_length = self.buffer.get_prev_line_length(self.cursor_row)
+                self.cursor_row -= 1
+                self.cursor_col = prev_line_length
+        elif key == curses.KEY_RIGHT:
+            if self.cursor_col < len(self.buffer.lines[self.cursor_row]):
+                self.cursor_col += 1
+            else:
+                self.cursor_row += 1
+                self.cursor_col = 0
 
         # Clamp horizontal cursor if line is shorter
         current_line = self.buffer.lines[self.cursor_row]
