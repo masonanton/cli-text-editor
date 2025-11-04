bbf7b4c0c4803c24153b18835eed35adc7465f86
Mason Anton <masonanton1@gmail.com>
2025-11-04 09:55:35 -0600

    Remove commented code

--- editor.py
+++ editor.py
@@ -72,30 +72,6 @@ class Editor:
         self.stdscr.move(self.cursor.row, self.cursor.col)
         self.stdscr.refresh()
 
-    # def move_cursor(self, key):
-    #     if key == curses.KEY_UP and self.cursor_row > 0:
-    #         self.cursor_row -= 1
-    #     elif key == curses.KEY_DOWN and self.cursor_row < len(self.buffer.lines) - 1:
-    #         self.cursor_row += 1
-    #     elif key == curses.KEY_LEFT: 
-    #         if self.cursor_col > 0:
-    #             self.cursor_col -= 1
-    #         elif self.cursor_row > 0:
-    #             prev_line_length = self.buffer.get_prev_line_length(self.cursor_row)
-    #             self.cursor_row -= 1
-    #             self.cursor_col = prev_line_length
-    #     elif key == curses.KEY_RIGHT:
-    #         if self.cursor_col < len(self.buffer.lines[self.cursor_row]):
-    #             self.cursor_col += 1
-    #         elif self.cursor_row < len(self.buffer.lines) - 1:
-    #             self.cursor_row += 1
-    #             self.cursor_col = 0
-
-    #     # Clamp horizontal cursor if line is shorter
-    #     current_line = self.buffer.lines[self.cursor_row]
-    #     if self.cursor_col > len(current_line):
-    #         self.cursor_col = len(current_line)
-
     def prompt_filename(self, prompt_text="Save as: "):
         curses.echo()  # show typed characters
         max_y, _ = self.stdscr.getmaxyx()
